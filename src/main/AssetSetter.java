66174d137cbdac0854a0f6a0d05c25981a971289
Edmond <edmondtabianan@gmail.com>
2025-11-26 16:47:49 +0800

    new map

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -23,19 +23,18 @@ public class AssetSetter {
 
         gp.obj[3] = new object.OBJ_Door(gp);
         gp.obj[3].worldX = gp.TileSize * 10;
-        gp.obj[3].worldY = gp.TileSize * 11;
+        gp.obj[3].worldY = gp.TileSize * 12;
         gp.obj[4] = new object.OBJ_Door(gp);
         gp.obj[4].worldX = gp.TileSize * 8;
         gp.obj[4].worldY = gp.TileSize * 28;
         gp.obj[5] = new object.OBJ_Door(gp);
         gp.obj[5].worldX = gp.TileSize * 12;
-        gp.obj[5].worldY = gp.TileSize * 22;
+        gp.obj[5].worldY = gp.TileSize * 23;
         gp.obj[6] = new object.OBJ_Chest(gp);
         gp.obj[6].worldX = gp.TileSize * 10;
-        gp.obj[6].worldY = gp.TileSize * 7;
+        gp.obj[6].worldY = gp.TileSize * 9;
         gp.obj[7] = new object.OBJ_Boots(gp);
         gp.obj[7].worldX = gp.TileSize * 37;
         gp.obj[7].worldY = gp.TileSize * 42;
-        // not properly placed
     }
 }
